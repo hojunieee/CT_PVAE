f9bbd9d18717165dd3cc2f7e0d4d0df8fee6cee8
Hojune Kim <hojunek@login39.chn>
2023-06-16 14:18:11 -0700

    added padding to sinogram - tomopy format now

--- radon_torch/sinogram_class.py
+++ radon_torch/sinogram_class.py
@@ -35,14 +35,17 @@ class ImgToSinogram(nn.Module):
         n, k, y, x = image_batch.shape
 
         # Initialize a tensor to store the sinograms
-        sinograms = torch.zeros((n, k, x), device=image_batch.device)
+        padding = 28
+        padding_tensor = torch.zeros(padding)
+        sinograms = torch.zeros((n, k, x + 2 * padding), device=image_batch.device)
 
         for i in range(n):
-            sinogram = torch.zeros((k, x), device=image_batch.device)
+            sinogram = torch.zeros((k, x + 2 * padding), device=image_batch.device)
             for j in range(k):
                 image = image_batch[i, j]  # Get current image
                 line_integrated = torch.sum(image, dim=1)  # Line integration along the y-axis
-                sinogram[j, :] = line_integrated
+                line_integrated_padded = torch.cat((padding_tensor, line_integrated, padding_tensor))
+                sinogram[j] = line_integrated_padded
             sinograms[i] = sinogram
 
         self.sinogram_batch = sinograms
