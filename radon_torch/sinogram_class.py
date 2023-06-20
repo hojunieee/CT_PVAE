bc9b65d1a8429d33cbedec966d18d50b591b0a68
Hojune Kim <84814259+hojunieee@users.noreply.github.com>
2023-06-20 13:26:21 -0700

    trying to sync with NERSC

--- radon_torch/sinogram_class.py
+++ radon_torch/sinogram_class.py
@@ -1,8 +1,7 @@
 import torch
 import torch.nn as nn
-import torchvision.transforms.functional as F
+from torchvision.transforms.functional import rotate, InterpolationMode
 
-# Class Definition
 class ImageRotator(nn.Module):
     def __init__(self):
         super(ImageRotator, self).__init__()
@@ -21,7 +20,7 @@ class ImageRotator(nn.Module):
         for i in range(n):
             image = image_batch[i]  # Get current image
             for j in range(k):
-                image_rotated = F.rotate(image, angles[j])  # Rotate the image
+                image_rotated = rotate(image, -float(angles[j]), interpolation=InterpolationMode.BILINEAR)  # Rotate the image in the opposite direction
                 rotated_imgs[i][j] = image_rotated
         self.rotated_batch = rotated_imgs
 
@@ -36,15 +35,17 @@ class ImgToSinogram(nn.Module):
         n, k, y, x = image_batch.shape
 
         # Initialize a tensor to store the sinograms
-        sinograms = torch.zeros((n, k, x), device=image_batch.device)
+        padding = 28
+        padding_tensor = torch.zeros(padding)
+        sinograms = torch.zeros((n, k, x + 2 * padding), device=image_batch.device)
 
         for i in range(n):
-            sinogram = torch.zeros((k, x), device=image_batch.device)
+            sinogram = torch.zeros((k, x + 2 * padding), device=image_batch.device)
             for j in range(k):
                 image = image_batch[i, j]  # Get current image
                 line_integrated = torch.sum(image, dim=1)  # Line integration along the y-axis
-                sinogram[j, :] = line_integrated
+                line_integrated_padded = torch.cat((padding_tensor, line_integrated, padding_tensor))
+                sinogram[j] = line_integrated_padded
             sinograms[i] = sinogram
 
         self.sinogram_batch = sinograms
-###############################################################
